a8c42d310c16845ac67d56c05732c4b5519f42ea
Iva Stanisic <iva.stanisic01@yahoo.com>
2023-01-10 15:13:41 +0100

    preimenovana kolona indeks u broj_indeksa

---
+++ domaci/database/migrations/2023_01_10_124450_update_column_indeks_to_broj_indeksa_in_students.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+           $table->renameColumn('indeks','broj_indeksa');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->renameColumn('broj_indeksa','indeks');
+        });
+    }
+};
